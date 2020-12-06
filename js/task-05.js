95a4dee0b38f983344b5449dba50be0495b9e7ac
Katerina Kucheriavenko <ekucheryavenko0925@gmail.com>
2020-12-07 00:03:09 +0200

    improves code

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,5 @@ const inputRef = document.querySelector("#name-input");
 const nameLabelRef = document.querySelector("#name-output");
 
 inputRef.addEventListener("input", (event) => {
-  event.target.value !== "" ? (nameLabelRef.textContent = event.target.value) : (nameLabelRef.textContent = "незнакомец");
+  nameLabelRef.textContent = event.target.value !== "" ? event.target.value : "незнакомец";
 });
